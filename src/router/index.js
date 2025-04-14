8696cf72574e067d528039d952fa03bb45ecb390
JRogula <jakub.rogula@microsoft.wsei.edu.pl>
2025-04-14 14:25:48 +0200

    gh pages

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/TrainMe/'), // Update this line with your repo name
   routes
 })
 
